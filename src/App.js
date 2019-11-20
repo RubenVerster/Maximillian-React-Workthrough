325dd0820462d99a6b31f47003b20a99d69e8a87
Ruben Verster <rrverster31415@gmail.com>
2019-11-20 15:12:43 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    persons: [
+      { name: 'Max', age: 27 },
+      { name: 'Steve', age: 44 },
+      { name: 'Beth', age: 90 }
+    ],
+    otherState: 'some values',
+    showPersons: false
+  };
+
+  switchUserHandler = newName => {
+    this.setState({
+      persons: [
+        { name: newName, age: 27 },
+        { name: 'Steve', age: 44 },
+        { name: 'Beth', age: 90 }
+      ]
+      //setState doesn't copy the other data from the previous state. So make sure you add other state elements in the new state
+      // personState comes from the array destructuring done above
+      // otherState: personsState.otherState
+    });
+    // const [otherState, setOtherState] = useState('some other values');
+  };
+
+  nameChangedHandler = event => {
+    this.setState({
+      persons: [
+        { name: 'Max', age: 27 },
+        { name: event.target.value, age: 44 },
+        { name: 'Beth', age: 90 }
+      ]
+    });
+  };
+
+  togglePersonDisplayHandler = () => {};
+
+  render() {
+    const style = {
+      backGround: 'white',
+      font: 'inherit',
+      border: '1px soild blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+    return (
+      <div className="App">
+        <h1>This is my React App</h1>
+        <button style={style} onClick={this.togglePersonDisplay}>
+          Switch Name
+        </button>
+        <div>
+          <Person
+            name={this.state.persons[0].name}
+            age={this.state.persons[0].age}
+          />
+          <Person
+            name={this.state.persons[1].name}
+            age={this.state.persons[1].age}
+            // here we use a property to bind an onClick event with this component
+            click={this.switchUserHandler.bind(this, 'Max!!')}
+            change={this.nameChangedHandler}
+          >
+            My Hobbies: Driving
+          </Person>
+          <Person
+            name={this.state.persons[2].name}
+            age={this.state.persons[2].name}
+          />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Person/Person.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Person.css';
+
+const Person = props => {
+  return (
+    <div className="Person">
+      <p onClick={props.click}>
+        I'm {props.name} and I am {props.age}
+      </p>
+      <p>{props.children}</p>
+      {/* two way binding using props passed from state changes */}
+      <input type="text" value={props.name} onChange={props.change} />
+    </div>
+  );
+};
+
+export default Person;
